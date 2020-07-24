c2339c4af0ec7072d26517a77f421ae459ea516c
MatSaf123 <xnerov13@gmail.com>
2020-07-24 11:41:12 +0200

    created Retrofit instance

---
+++ app/src/main/java/com/matsaf123/newsapiapp/ui/Source.kt
@@ -0,0 +1,6 @@
+package com.matsaf123.newsapiapp.ui
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/matsaf123/newsapiapp/ui/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.matsaf123.newsapiapp.ui.util
+
+import com.matsaf123.newsapiapp.R
+
+class Constants {
+    companion object {
+        const val API_KEY = R.string.API_KEY_s.toString()  //  it's safer than putting the API key directly in here (?)
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
