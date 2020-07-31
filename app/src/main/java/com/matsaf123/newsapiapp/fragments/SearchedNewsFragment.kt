bac0a70070f21f72dd52023772c70f435ba5480a
MatSaf123 <xnerov13@gmail.com>
2020-07-31 15:36:19 +0200

    implemented searching news

--- app/src/main/java/com/matsaf123/newsapiapp/fragments/SearchedNewsFragment.kt
+++ app/src/main/java/com/matsaf123/newsapiapp/fragments/SearchedNewsFragment.kt
@@ -1,19 +1,85 @@
 package com.matsaf123.newsapiapp.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.matsaf123.newsapiapp.R
 import com.matsaf123.newsapiapp.ui.NewsActivity
 import com.matsaf123.newsapiapp.ui.NewsViewModel
+import com.matsaf123.newsapiapp.util.Resource
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.matsaf123.newsapiapp.adapters.NewsAdapter
+import com.matsaf123.newsapiapp.util.Constants.Companion.SEARCH_NEWS_TIME_DELAY
+import kotlinx.android.synthetic.main.fragment_search_news.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 class SearchedNewsFragment : Fragment(R.layout.fragment_search_news) {
 
     lateinit var viewModel : NewsViewModel
+    lateinit  var newsAdapter: NewsAdapter
+    val TAG = "SearchedNewsFragment"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
 
+        // delay when searching:
+        var  job: Job? = null
+        etSearch.addTextChangedListener {editable ->
+            job?.cancel()
+            job = MainScope().launch {
+                delay(SEARCH_NEWS_TIME_DELAY)
+                editable?.let {
+                    if(editable.toString().isNotEmpty()) {
+                        viewModel.searchNews(editable.toString())
+                    }
+                }
+            }
+        }
+
+        viewModel.searchNews.observe(viewLifecycleOwner, Observer { response ->
+            when(response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let {newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let {message ->
+                        Log.e(TAG,"An error occured: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvSearchNews.apply {
+            adapter = newsAdapter
+            layoutManager =  LinearLayoutManager(activity)
+        }
+    }
+
+    private fun hideProgressBar() {
+        paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
     }
 }
--- app/src/main/java/com/matsaf123/newsapiapp/util/Constants.kt
+++ app/src/main/java/com/matsaf123/newsapiapp/util/Constants.kt
@@ -7,5 +7,6 @@ class Constants {
         //const val API_KEY = R.string.API_KEY_s.toString()  //  it's safer than putting the API key directly in here (?)
         const val API_KEY = "5f223882025a48899f99996fe26b748f"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
